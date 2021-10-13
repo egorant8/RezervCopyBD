e5c9ba66b6d30bf6b15d089fb391c190e41d68ee
egorant8 <51228266+egorant8@users.noreply.github.com>
2021-10-13 09:03:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace AppRezervCopy_CrownBD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            while (true)
+            {
+                int cc = DateTime.Now.Hour;
+                try
+                {
+                    if (File.Exists(@"D:\crownrezerv\" + cc + @"h\crownBD.mdb"))
+                    {
+                        File.Delete(@"D:\crownrezerv\" + cc + @"h\crownBD.mdb");
+                        File.Copy(@"D:\Crown2021\crownBD.mdb", @"D:\crownrezerv\" + cc + @"h\crownBD.mdb");
+                    }
+                    else
+                    {
+                        File.Copy(@"D:\Crown2021\crownBD.mdb", @"D:\crownrezerv\" + cc + @"h\crownBD.mdb");
+                    }
+                }
+                catch (Exception ee)
+                {
+                    Console.WriteLine(ee.ToString());
+                }
+                Thread.Sleep(3600 * 1000);
+            }
+        }
+    }
+}
